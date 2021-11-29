2e3cd14e3c3aac1f6c530b3295ec3daa29cef11f
mp4 <puleri.mp4@gmail.com>
2021-11-29 13:38:32 -0800

    Initial commit

---
+++ src/App.svelte
@@ -0,0 +1,24 @@
+<script>
+	import FeedbackList from './components/FeedbackList.svelte';
+	let feedback = [
+		{
+			id: 1,
+			rating: 10,
+			text: 'Lorem ad eu ut cillum ad do excepteur elit. Irure elit labore adipisicing minim. Lorem non consectetur et eiusmod elit magna nisi commodo laborum velit. Nulla esse esse eu labore duis consectetur nisi est veniam ad reprehenderit aliquip sint. Consequat consequat nisi et sint nulla eiusmod aliqua aute fugiat magna. Nostrud nostrud sunt aliqua laboris exercitation magna.',
+		},
+		{
+			id: 2,
+			rating: 9,
+			text: 'Lorem ad eu ut cillum ad do excepteur elit. Irure elit labore adipisicing minim. Lorem non consectetur et eiusmod elit magna nisi commodo laborum velit. Nulla esse esse eu labore duis consectetur nisi est veniam ad reprehenderit aliquip sint. Consequat consequat nisi et sint nulla eiusmod aliqua aute fugiat magna. Nostrud nostrud sunt aliqua laboris exercitation magna.',
+		},
+		{
+			id: 3,
+			rating: 8,
+			text: 'Lorem ad eu ut cillum ad do excepteur elit. Irure elit labore adipisicing minim. Lorem non consectetur et eiusmod elit magna nisi commodo laborum velit. Nulla esse esse eu labore duis consectetur nisi est veniam ad reprehenderit aliquip sint. Consequat consequat nisi et sint nulla eiusmod aliqua aute fugiat magna. Nostrud nostrud sunt aliqua laboris exercitation magna.',
+		},
+	]
+</script>
+
+<main>
+	<FeedbackList {feedback} />
+</main>
